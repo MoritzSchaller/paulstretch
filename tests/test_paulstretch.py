aaaffbfe461b1a13ec74e245931d539e8eac4a40
Moritz Schaller <kontakt@moritz-schaller.de>
2025-07-16 12:39:59 +0200

    Add test cases

--- tests/test_paulstretch.py
+++ tests/test_paulstretch.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import pytest
+
 import numpy as np
 import soundfile as sf
 
@@ -5,11 +9,28 @@ import paulstretch as ps
 
 
 
+audio_files = Path("tests/audio").glob("*")
+stretch_factors = (0.23, 1, 11, 336.2)
+data_types = ("float64", "float32")
+
+    
 def test_call():
     y_in = np.zeros((2,10000), dtype=np.float32)
     sr = 44100
     factor = 10
 
     y_out = ps.stretch(y_in, sr, factor)
+
+    assert isinstance(y_out, np.ndarray), "Output should be a numpy array"
+
+
+@pytest.mark.parametrize("audio_file", audio_files)
+@pytest.mark.parametrize("data_type", data_types)
+@pytest.mark.parametrize("stretch_factor", stretch_factors)
+def test_stretch(audio_file, data_type, stretch_factor):
+    
+    y, sr = sf.read(audio_file, dtype=data_type)
+    
+    y_out = ps.stretch(y=y, sr=sr, stretch_factor=stretch_factor)
 
     assert isinstance(y_out, np.ndarray), "Output should be a numpy array"
